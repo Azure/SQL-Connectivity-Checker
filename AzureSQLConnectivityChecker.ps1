01620c70b98e4eb9f3fedf81d3834be842018a4f
Milos Curcic <v-micurc@microsoft.com>
2020-06-01 16:31:22 +0200

    Removed elevated permissions check for cross platform support.

--- AzureSQLConnectivityChecker.ps1
+++ AzureSQLConnectivityChecker.ps1
@@ -534,10 +534,10 @@ function RunConnectivityPolicyTests($port) {
     Write-Host
     Write-Host 'Advanced connectivity policy tests:' -ForegroundColor Green
 
-    if (!$CustomerRunningInElevatedMode) {
-        Write-Host ' Powershell must be run as an administrator to run advanced connectivity policy tests!' -ForegroundColor Yellow
-        return
-    }
+    #if (!$CustomerRunningInElevatedMode) {
+    #    Write-Host ' Powershell must be run as an administrator to run advanced connectivity policy tests!' -ForegroundColor Yellow
+    #    return
+    #}
 
     if ($(Get-ExecutionPolicy) -eq 'Restricted') {
         Write-Host ' Advanced connectivity policy tests cannot be run because of current execution policy (Restricted)!' -ForegroundColor Yellow
